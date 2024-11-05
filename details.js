423d4a5395ce8940c964b474eb42e3f025891108
aromalsur <aromalsureshr@gmail.com>
2024-11-05 22:24:57 +0530

    05/11/24-2v

--- details.js
+++ details.js
@@ -9,10 +9,6 @@ function selectBHK() {
 
 function selectFurnishing(furnishing) {
     selectedFurnishing = furnishing;
-    // Highlight selected furnishing option (optional)
-    const furnishingItems = document.querySelectorAll('.furnishing-item');
-    furnishingItems.forEach(item => item.classList.remove('selected'));
-    event.currentTarget.classList.add('selected'); // Highlight selected
 }
 
 function selectPropertyType() {
@@ -21,19 +17,15 @@ function selectPropertyType() {
 }
 
 function goToPredictionPage() {
-    // Show loading overlay
     document.getElementById('loading-overlay').style.display = 'flex';
 
-    // Check if all details are filled
     if (!selectedBHK || !selectedFurnishing || !selectedPropertyType) {
         alert("Please fill in all the details");
-        document.getElementById('loading-overlay').style.display = 'none'; // Hide loading overlay
+        document.getElementById('loading-overlay').style.display = 'none';
         return;
     }
 
-    // Simulate loading time before redirecting
     setTimeout(() => {
-        // Redirect to the prediction page
         window.location.href = 'prediction.html';
-    }, 2000); // 2 seconds delay
+    }, 2000);
 }
